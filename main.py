9f7dda145cf4471805b2964ba7a81027d76b87d4
Daniel-Nuruulu <danieldoni236@gmail.com>
2025-08-25 17:59:19 +0600

    Жаны кошулдум

--- main.py
+++ main.py
@@ -16,4 +16,15 @@ class Umarchik:
         return self.name
 
 D = Umarchik('Umar')
-print(D.get_info())
+print(D.get_info()
+
+
+class Danchik:
+    def __init__(self, name):
+        self.name = name
+
+    def get_info(self):
+        return self.name
+
+D = Danchik('Danielchik')
+print(D.get_info()
